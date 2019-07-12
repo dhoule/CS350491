b4eed3376d0ccc96005c5f38a9d4a96a5368d3c5
dhoule <caine2003@gmail.com>
2019-07-11 19:31:24 -0500

    Updates Footer.js
    Just adds comments.

--- javascripts/Footer.js
+++ javascripts/Footer.js
@@ -6,6 +6,11 @@
 * pages.
 ***********************************************/
 
+/* 
+	Hardcoded list of view directories. 
+	Must be exactly the same as the directory names.
+	Only letters and underscores are allowed.
+*/
 var directories = [
 	'Feedback',
 	'Lil_Story',
@@ -13,6 +18,7 @@ var directories = [
 	'Weekly_Schedule'
 ];
 
+// Changes the class value of the navbar element
 function modifyNavbar() {
   var menu = document.getElementById("menuNavBar");
   if (menu.className === "navbar") {
@@ -20,27 +26,40 @@ function modifyNavbar() {
   } else {
     menu.className = "navbar";
   }
-}
+} // end modifyNavbar
 
+/* 
+	Actually builds the innerHTML of the navbar on the `page` that calls this function.
+	This is an onload even listener function, on the body element in the pages.
+	The name of the directory the view is in must be give, along with the event.
+*/
 function buildNavbarMenu(e,page) {
 	var menu = document.getElementById("menuNavBar");
+	// Except for the 'main', all of the relative paths start the same way 
 	var firstPart = (page == 'main') ? '<a class="button" href="./views/': '<a class="button" href="../';
+	// For all but the 'main' page, the 'main' page needs to be the first button.
 	var temp = (page == 'main') ? '' : getMainButton();
+	// go over the directories
 	for (let directory of directories) {
+		// No sense in have a link to the current page, on the current page
 		if (page != directory) {
+			// Build each button <a> element
 			temp += firstPart + directory + '/index.htm">' + directory.replace('_', ' ') + '</a>';
 		}
 	}
+	// Add the last element 
 	temp += getLastButton();
-	console.log(temp);
+	// Adds the innHTML to the navbar
 	menu.innerHTML = temp;
-}
+} // end buildNavbarMenu
 
+// This is just to take it out of the calling function
 function getMainButton() {
 	return '<a class="button" href="../../index.htm">Main page</a>';
-}
+} // end getMainButton
 
+// This just takes the string out of the calling function
 function getLastButton() {
 	return '<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="modifyNavbar()">&#9776;</a>';
-}
+} // end getLastButton
 
